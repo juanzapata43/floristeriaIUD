76d2d373734642682b111c8cf1e5773083f96767
juanzapata43 <149023876+juanzapata43@users.noreply.github.com>
2024-11-11 22:02:29 -0500

    Add files via upload

---
+++ src/main/java/edu/iud/floristeriaIUD/FloristeriaIUDApplication.java
@@ -0,0 +1,14 @@
+package edu.iud.floristeriaIUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristeriaIUDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloristeriaIUDApplication.class, args);
+		System.out.println("app cargado correctamente");
+	}
+
+}
